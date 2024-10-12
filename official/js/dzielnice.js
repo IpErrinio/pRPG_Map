41d6c43ce7c7061b1b5877ddd94cc916ea8a2984
IpErrinio <101106107+IpErrinio@users.noreply.github.com>
2024-10-12 04:28:53 +0200

    stabilna wersja

---
+++ official/js/dzielnice.js
@@ -0,0 +1,542 @@
+var dzielnice = {
+  // Los Santos
+  "Los Santos Airport": {
+    koordynaty: przeksztalcNaWspolrzedne(1230, 125),
+  },
+  Commerce: {
+    koordynaty: przeksztalcNaWspolrzedne(1180, 380),
+  },
+  "Conference Center": {
+    koordynaty: przeksztalcNaWspolrzedne(1075, 310),
+  },
+  "Downtown Los Santos": {
+    koordynaty: przeksztalcNaWspolrzedne(1190, 440),
+  },
+  "East Beach": {
+    koordynaty: przeksztalcNaWspolrzedne(1490, 360),
+  },
+  "East Los Santos": {
+    koordynaty: przeksztalcNaWspolrzedne(1398, 410),
+  },
+  "El Corona": {
+    koordynaty: przeksztalcNaWspolrzedne(1245, 238),
+  },
+  Ganton: {
+    koordynaty: przeksztalcNaWspolrzedne(1400, 320),
+  },
+  "Glen Park": {
+    koordynaty: przeksztalcNaWspolrzedne(1270, 470),
+  },
+  Idlewood: {
+    koordynaty: przeksztalcNaWspolrzedne(1290, 345),
+  },
+  Jefferson: {
+    koordynaty: przeksztalcNaWspolrzedne(1330, 438),
+  },
+  "Las Colinas": {
+    koordynaty: przeksztalcNaWspolrzedne(1390, 510),
+  },
+  "Los Flores": {
+    koordynaty: przeksztalcNaWspolrzedne(1457, 440),
+  },
+  "Los Santos Inlet": {
+    koordynaty: przeksztalcNaWspolrzedne(1225, 313),
+  },
+  "Little Mexico": {
+    koordynaty: przeksztalcNaWspolrzedne(1225, 313),
+  },
+  Maryna: {
+    //do naprawy
+    koordynaty: przeksztalcNaWspolrzedne(960, 350),
+  },
+  Market: {
+    koordynaty: przeksztalcNaWspolrzedne(1060, 430),
+  },
+  Mulholland: {
+    koordynaty: przeksztalcNaWspolrzedne(1060, 565),
+  },
+  "Mulholland Intersection": {
+    koordynaty: przeksztalcNaWspolrzedne(1195, 540),
+  },
+  "Ocean Docs": {
+    koordynaty: przeksztalcNaWspolrzedne(1430, 170),
+  },
+  "Pershing Square": {
+    koordynaty: przeksztalcNaWspolrzedne(1160, 335),
+  },
+  "Playa del Seville": {
+    koordynaty: przeksztalcNaWspolrzedne(1500, 250),
+  },
+  Richman: {
+    koordynaty: przeksztalcNaWspolrzedne(875, 490),
+  },
+  Rodeo: {
+    koordynaty: przeksztalcNaWspolrzedne(870, 380),
+  },
+  "Santa Marina Beach": {
+    koordynaty: przeksztalcNaWspolrzedne(863, 280),
+  },
+  "Temple/Sunrise": {
+    koordynaty: przeksztalcNaWspolrzedne(1070, 500),
+  },
+  "Unity Station": {
+    koordynaty: przeksztalcNaWspolrzedne(1222, 265),
+  },
+  "Verona Beach": {
+    koordynaty: przeksztalcNaWspolrzedne(990, 280),
+  },
+  Vinewood: {
+    koordynaty: przeksztalcNaWspolrzedne(980, 470),
+  },
+  Willowfield: {
+    koordynaty: przeksztalcNaWspolrzedne(1380, 270),
+  },
+  "Verdant Bluffs": {
+    koordynaty: przeksztalcNaWspolrzedne(1125, 260),
+  },
+
+  // San Fierro
+  "Battery Point": {
+    koordynaty: przeksztalcNaWspolrzedne(90, 1133),
+  },
+  "Calton Heights": {
+    koordynaty: przeksztalcNaWspolrzedne(292, 1145),
+  },
+  "China town": {
+    koordynaty: przeksztalcNaWspolrzedne(210, 945),
+  },
+  "City Hall": {
+    koordynaty: przeksztalcNaWspolrzedne(68, 869),
+  },
+  Doherty: {
+    koordynaty: przeksztalcNaWspolrzedne(258, 747),
+  },
+  Downtown: {
+    koordynaty: przeksztalcNaWspolrzedne(308, 949),
+  },
+  "Easter Basin": {
+    koordynaty: przeksztalcNaWspolrzedne(380, 889),
+  },
+  "Easter Bay Airport": {
+    koordynaty: przeksztalcNaWspolrzedne(400, 711),
+  },
+  "Esplanade East": {
+    koordynaty: przeksztalcNaWspolrzedne(392, 1073),
+  },
+  "Esplanade North": {
+    koordynaty: przeksztalcNaWspolrzedne(284, 1153),
+  },
+  Financial: {
+    koordynaty: przeksztalcNaWspolrzedne(312, 1035),
+  },
+  "Foster Valley": {
+    koordynaty: przeksztalcNaWspolrzedne(258, 591),
+  },
+  Garcia: {
+    koordynaty: przeksztalcNaWspolrzedne(180, 797),
+  },
+  "Garver Bridge": {
+    koordynaty: przeksztalcNaWspolrzedne(446, 1041),
+  },
+  Hashbury: {
+    koordynaty: przeksztalcNaWspolrzedne(124, 763),
+  },
+  Paradiso: {
+    koordynaty: przeksztalcNaWspolrzedne(91, 1053),
+  },
+  "Juniper Hill": {
+    koordynaty: przeksztalcNaWspolrzedne(154, 991),
+  },
+  "Juniper Hollow": {
+    koordynaty: przeksztalcNaWspolrzedne(154, 1092),
+  },
+  "Kincaid Bridge": {
+    koordynaty: przeksztalcNaWspolrzedne(473, 1002),
+  },
+  Kings: {
+    koordynaty: przeksztalcNaWspolrzedne(223, 891),
+  },
+  "Ocean Flats": {
+    koordynaty: przeksztalcNaWspolrzedne(52, 798),
+  },
+  Palisades: {
+    koordynaty: przeksztalcNaWspolrzedne(28, 1020),
+  },
+  Queens: {
+    koordynaty: przeksztalcNaWspolrzedne(146, 894),
+  },
+  "Santa Flora": {
+    koordynaty: przeksztalcNaWspolrzedne(92, 955),
+  },
+  "Cranberry Station": {
+    koordynaty: przeksztalcNaWspolrzedne(266, 825),
+  },
+  "Avispa Country Club": {
+    koordynaty: przeksztalcNaWspolrzedne(118, 700),
+  },
+  "Missionary Hill": {
+    koordynaty: przeksztalcNaWspolrzedne(107, 630),
+  },
+  "Easter Tunnel": {
+    koordynaty: przeksztalcNaWspolrzedne(364, 571),
+  },
+
+  // Las Venturas
+  Blackfield: {
+    koordynaty: przeksztalcNaWspolrzedne(1052, 1157),
+  },
+  "Blackfield College": {
+    koordynaty: przeksztalcNaWspolrzedne(1046, 1066),
+  },
+  "Blackfield Chapel": {
+    koordynaty: przeksztalcNaWspolrzedne(1136, 972),
+  },
+  "Blackfield Intersection": {
+    koordynaty: przeksztalcNaWspolrzedne(1100, 1032),
+  },
+  Creek: {
+    koordynaty: przeksztalcNaWspolrzedne(1502, 1376),
+  },
+  "Julius Thruway East": {
+    koordynaty: przeksztalcNaWspolrzedne(1472, 1277),
+  },
+  "Julius Thruway North": {
+    koordynaty: przeksztalcNaWspolrzedne(1370, 1451),
+  },
+  "Julius Thruway West": {
+    koordynaty: przeksztalcNaWspolrzedne(1086, 1229),
+  },
+  "Julius Thruway South": {
+    koordynaty: przeksztalcNaWspolrzedne(1306, 991),
+  },
+  "Las Venturas Airport": {
+    koordynaty: przeksztalcNaWspolrzedne(1168, 1183),
+  },
+  "Redsands East": {
+    koordynaty: przeksztalcNaWspolrzedne(1280, 1334),
+  },
+  "Redsands West": {
+    koordynaty: przeksztalcNaWspolrzedne(1166, 1340),
+  },
+  "Rockshore East": {
+    koordynaty: przeksztalcNaWspolrzedne(1472, 1003),
+  },
+  "Rockshore West": {
+    koordynaty: przeksztalcNaWspolrzedne(1346, 969),
+  },
+  "Spiny bed": {
+    koordynaty: przeksztalcNaWspolrzedne(1367, 1496),
+  },
+  "The Strip": {
+    koordynaty: przeksztalcNaWspolrzedne(1302, 1127),
+  },
+  "Yellow Bell Golf Club": {
+    koordynaty: przeksztalcNaWspolrzedne(1115, 1511),
+  },
+  "Yellow Bell Station": {
+    koordynaty: przeksztalcNaWspolrzedne(1142, 1463),
+  },
+  "Prickle Pine": {
+    koordynaty: przeksztalcNaWspolrzedne(1226, 1490),
+  },
+  "KACC Military Fuels": {
+    koordynaty: przeksztalcNaWspolrzedne(1448, 1505),
+  },
+  "Pilson Intersection": {
+    koordynaty: przeksztalcNaWspolrzedne(1087, 1405),
+  },
+  "White Wood Estates": {
+    koordynaty: przeksztalcNaWspolrzedne(1040, 1322),
+  },
+  "The Emerald Isle": {
+    koordynaty: przeksztalcNaWspolrzedne(1322, 1397),
+  },
+  "Roca Escalante": {
+    koordynaty: przeksztalcNaWspolrzedne(1409, 1391),
+  },
+  "Old Venturas Strip": {
+    koordynaty: przeksztalcNaWspolrzedne(1400, 1328),
+  },
+  "The Visage": {
+    koordynaty: przeksztalcNaWspolrzedne(1276, 1275),
+  },
+  "Harry Gold Parkway": {
+    koordynaty: przeksztalcNaWspolrzedne(1234, 1287),
+  },
+  "Starfish Casino": {
+    koordynaty: przeksztalcNaWspolrzedne(1400, 1283),
+  },
+  "The Clown's Pocket": {
+    koordynaty: przeksztalcNaWspolrzedne(1362, 1247),
+  },
+  "Pirates In Men's Pants": {
+    koordynaty: przeksztalcNaWspolrzedne(1266, 1207),
+  },
+  "Caligula's Palace": {
+    koordynaty: przeksztalcNaWspolrzedne(1362, 1221),
+  },
+  Pilgrim: {
+    koordynaty: przeksztalcNaWspolrzedne(1432, 1189),
+  },
+  "Sobell Rail Yards": {
+    koordynaty: przeksztalcNaWspolrzedne(1506, 1249),
+  },
+  "The High Roller": {
+    koordynaty: przeksztalcNaWspolrzedne(1266, 1143),
+  },
+  "Royal Casino": {
+    koordynaty: przeksztalcNaWspolrzedne(1354, 1157),
+  },
+  "The Camel's Toe": {
+    koordynaty: przeksztalcNaWspolrzedne(1380, 1117),
+  },
+  "The Pink Swan": {
+    koordynaty: przeksztalcNaWspolrzedne(1266, 1095),
+  },
+  "Lva Freight Depot": {
+    koordynaty: przeksztalcNaWspolrzedne(1182, 1061),
+  },
+  "Randolph Industrial Estate": {
+    koordynaty: przeksztalcNaWspolrzedne(1206, 981),
+  },
+  "Last Dime Motel": {
+    koordynaty: przeksztalcNaWspolrzedne(1262, 979),
+  },
+  "Four Dragons Casino": {
+    koordynaty: przeksztalcNaWspolrzedne(1270, 1047),
+  },
+  "Come A Lot": {
+    koordynaty: przeksztalcNaWspolrzedne(1378, 1063),
+  },
+  "Linden Side": {
+    koordynaty: przeksztalcNaWspolrzedne(1502, 1069),
+  },
+  "Linden Station": {
+    koordynaty: przeksztalcNaWspolrzedne(1502, 1127),
+  },
+
+  // Bone Country
+  "Las Payasdas": {
+    koordynaty: przeksztalcNaWspolrzedne(707, 1481),
+  },
+  "Las Brujas": {
+    koordynaty: przeksztalcNaWspolrzedne(692, 1340),
+  },
+  "El Castillo Del Diablo": {
+    koordynaty: przeksztalcNaWspolrzedne(713, 1394),
+  },
+  "Verdant Meadow Aircraft Graveyard": {
+    koordynaty: przeksztalcNaWspolrzedne(830, 1445),
+  },
+  "Restricted Area/No Fly Zone(Baza wojskowa)/Area 69": {
+    koordynaty: przeksztalcNaWspolrzedne(815, 1274),
+  },
+  "Regular Tom": {
+    koordynaty: przeksztalcNaWspolrzedne(684, 1255),
+  },
+  "The Big Ear": {
+    koordynaty: przeksztalcNaWspolrzedne(698, 1183),
+  },
+  "Lil' Probe Inn": {
+    koordynaty: przeksztalcNaWspolrzedne(780, 1167),
+  },
+  "Green Palms": {
+    koordynaty: przeksztalcNaWspolrzedne(836, 1155),
+  },
+  "Octane Springs": {
+    koordynaty: przeksztalcNaWspolrzedne(898, 1159),
+  },
+  "FC Fort Carson": {
+    koordynaty: przeksztalcNaWspolrzedne(742, 1051),
+  },
+  "Hunter Quarry": {
+    koordynaty: przeksztalcNaWspolrzedne(928, 1015),
+  },
+
+  // Tierra Robada
+  "Gant Bridge": {
+    koordynaty: przeksztalcNaWspolrzedne(80, 1265),
+  },
+  "San Fierro Bay": {
+    koordynaty: przeksztalcNaWspolrzedne(174, 1259),
+  },
+  Bayside: {
+    koordynaty: przeksztalcNaWspolrzedne(116, 1415),
+  },
+  "Bayside Marina": {
+    koordynaty: przeksztalcNaWspolrzedne(194, 1403),
+  },
+  "Bayside Tunnel": {
+    koordynaty: przeksztalcNaWspolrzedne(227, 1463),
+  },
+  "El Quebrados": {
+    koordynaty: przeksztalcNaWspolrzedne(383, 1475),
+  },
+  "Alda Malvada": {
+    koordynaty: przeksztalcNaWspolrzedne(431, 1451),
+  },
+  "Valle Ocultado": {
+    koordynaty: przeksztalcNaWspolrzedne(557, 1496),
+  },
+  "Arco Del Oeste": {
+    koordynaty: przeksztalcNaWspolrzedne(587, 1406),
+  },
+  "The Sherman Dam/Tama": {
+    koordynaty: przeksztalcNaWspolrzedne(587, 1310),
+  },
+  "Sherman Reservoir": {
+    koordynaty: przeksztalcNaWspolrzedne(590, 1256),
+  },
+  "Las Barrancas": {
+    koordynaty: przeksztalcNaWspolrzedne(562, 1181),
+  },
+  "Robada Intersection": {
+    koordynaty: przeksztalcNaWspolrzedne(514, 1115),
+  },
+
+  // Whetstone
+  "MC Mount Chiliad": {
+    koordynaty: przeksztalcNaWspolrzedne(134, 387),
+  },
+  "Shady Creeks": {
+    koordynaty: przeksztalcNaWspolrzedne(366, 265),
+  },
+  "Shady Cabin": {
+    koordynaty: przeksztalcNaWspolrzedne(355, 197),
+  },
+  "AP Angel Pine": {
+    koordynaty: przeksztalcNaWspolrzedne(218, 169),
+  },
+
+  // Flint Country
+  "The Farm": {
+    koordynaty: przeksztalcNaWspolrzedne(500, 523),
+  },
+  "Beacon Hill": {
+    koordynaty: przeksztalcNaWspolrzedne(680, 520),
+  },
+  "Flint Range": {
+    koordynaty: przeksztalcNaWspolrzedne(671, 415),
+  },
+  "Flint Intersection": {
+    koordynaty: przeksztalcNaWspolrzedne(752, 422),
+  },
+  "Leafy Hollow": {
+    koordynaty: przeksztalcNaWspolrzedne(518, 347),
+  },
+  "Back O' Beyond": {
+    koordynaty: przeksztalcNaWspolrzedne(580, 209),
+  },
+  "Los Santos Inlet": {
+    koordynaty: przeksztalcNaWspolrzedne(739, 284),
+  },
+
+  // Red Country
+  "Easter Bay Chemicals": {
+    koordynaty: przeksztalcNaWspolrzedne(503, 622),
+  },
+  "Fallen Tree": {
+    koordynaty: przeksztalcNaWspolrzedne(612, 651),
+  },
+  "Flint Waters": {
+    koordynaty: przeksztalcNaWspolrzedne(713, 640),
+  },
+  "The Panopticon": {
+    koordynaty: przeksztalcNaWspolrzedne(608, 790),
+  },
+  "BB Blueberry Acres": {
+    koordynaty: przeksztalcNaWspolrzedne(746, 800),
+  },
+  "BB Blueberry": {
+    koordynaty: przeksztalcNaWspolrzedne(821, 707),
+  },
+  "Martini Bridge": {
+    koordynaty: przeksztalcNaWspolrzedne(730, 887),
+  },
+  "Fallow Bridge": {
+    koordynaty: przeksztalcNaWspolrzedne(904, 916),
+  },
+  "Hampton Barns": {
+    koordynaty: przeksztalcNaWspolrzedne(949, 865),
+  },
+  "Fern Ridge": {
+    koordynaty: przeksztalcNaWspolrzedne(1006, 782),
+  },
+  Dillimore: {
+    koordynaty: przeksztalcNaWspolrzedne(959, 646),
+  },
+  "Hilltop Farms": {
+    koordynaty: przeksztalcNaWspolrzedne(1051, 706),
+  },
+  Montgomery: {
+    koordynaty: przeksztalcNaWspolrzedne(1103, 863),
+  },
+  "Montgomery Intersection": {
+    koordynaty: przeksztalcNaWspolrzedne(1192, 863),
+  },
+  "The Mako Span": {
+    koordynaty: przeksztalcNaWspolrzedne(1216, 920),
+  },
+  "Fisher's Lagoon": {
+    koordynaty: przeksztalcNaWspolrzedne(1292, 761),
+  },
+  "PC Palomino Creek": {
+    koordynaty: przeksztalcNaWspolrzedne(1378, 797),
+  },
+  "San Andreas Sound": {
+    koordynaty: przeksztalcNaWspolrzedne(1442, 916),
+  },
+  "Hanky Panky Point": {
+    koordynaty: przeksztalcNaWspolrzedne(1460, 857),
+  },
+  "Northstar Rock": {
+    koordynaty: przeksztalcNaWspolrzedne(1423, 653),
+  },
+  "Frederick Bridge": {
+    koordynaty: przeksztalcNaWspolrzedne(1485, 891),
+  },
+
+  // Prace Dorywcze
+  Magazyn: {
+    koordynaty: przeksztalcNaWspolrzedne(188, 1382),
+  },
+  Grzybki: {
+    koordynaty: przeksztalcNaWspolrzedne(664, 324),
+  },
+  Drwal: {
+    koordynaty: przeksztalcNaWspolrzedne(641, 741),
+  },
+  Kurier: {
+    koordynaty: przeksztalcNaWspolrzedne(1478, 154),
+  },
+  "Kopalnia/Kopka": {
+    koordynaty: przeksztalcNaWspolrzedne(572, 1158),
+  },
+  "Kosiarki SF": {
+    koordynaty: przeksztalcNaWspolrzedne(276, 1061),
+  },
+  "Kosiarki LS": {
+    koordynaty: przeksztalcNaWspolrzedne(1163, 222),
+  },
+  "Sweepers SF": {
+    koordynaty: przeksztalcNaWspolrzedne(196, 851),
+  },
+  "Sweepers LV": {
+    koordynaty: przeksztalcNaWspolrzedne(1286, 1310),
+  },
+  "Sweepers LS": {
+    koordynaty: przeksztalcNaWspolrzedne(1129, 351),
+  },
+  "Kutry LV": {
+    koordynaty: przeksztalcNaWspolrzedne(1369, 919),
+  },
+  "Kutry LS": {
+    koordynaty: przeksztalcNaWspolrzedne(1355, 151),
+  },
+  Rybak: {
+    koordynaty: przeksztalcNaWspolrzedne(869, 241),
+  },
+  Dodo: {
+    koordynaty: przeksztalcNaWspolrzedne(1292, 190),
+  },
+};
---
+++ official/script.js
@@ -0,0 +1,100 @@
+// Funkcja do wyszukiwania dzielnic
+function searchDistrict() {
+  var searchTerm = document.getElementById("search").value.toLowerCase();
+
+  // Usuwanie istniejących markerów
+  map.eachLayer(function (layer) {
+    if (layer instanceof L.Marker) {
+      map.removeLayer(layer);
+    }
+  });
+
+  // Jeśli pole wyszukiwania jest puste, zakończ funkcję
+  if (!searchTerm) {
+    return;
+  }
+
+  var foundMarkers = [];
+  var bounds = L.latLngBounds(); // Tworzenie obiektu do zbierania granic
+
+  // Przeszukiwanie bazy dzielnic
+  for (var dzielnica in dzielnice) {
+    if (dzielnica.toLowerCase().includes(searchTerm.toLowerCase())) {
+      var coords = dzielnice[dzielnica].koordynaty;
+
+      // Dodanie markera w znalezionej dzielnicy
+      var marker = L.marker(coords)
+        .addTo(map)
+        .bindPopup(`<b>${dzielnica}</b>`)
+        .openPopup();
+      foundMarkers.push(marker); // Dodanie markera do tablicy
+      bounds.extend(coords); // Dodanie koordynatów do granic
+    }
+  }
+
+  if (foundMarkers.length === 1) {
+    map.setView(coords, 0);
+    // Jeśli jest tylko jeden wynik, zbliż na ten wynik
+  } else if (foundMarkers.length > 1) {
+    // Jeśli jest więcej wyników, wyśrodkuj mapę na wszystkie wyniki
+    map.fitBounds(bounds);
+  }
+}
+
+// Tworzenie mapy Leaflet z układem współrzędnych CRS Simple (dla obrazków)
+var map = L.map("map", {
+  crs: L.CRS.Simple, // Prosty CRS, bez geograficznych współrzędnych
+  minZoom: -1, // Dostosowanie zoomu
+  maxZoom: 1,
+  zoomControl: true,
+  maxBounds: [
+    [0, 0],
+    [1545, 1545],
+  ], // Granice mapy ustawione na rozmiar obrazu
+  maxBoundsViscosity: 1.0, // Zabezpiecza przed przesunięciem mapy poza granice
+});
+
+// Ustawienie granic mapy odpowiadających rozdzielczości obrazu (1545x1545 pikseli)
+var bounds = [
+  [0, 0],
+  [1545, 1545],
+]; // Granice obrazu to jego rzeczywiste rozmiary
+
+// Ustawienie obrazu mapy w pełnych wymiarach
+var image = L.imageOverlay("images/mapa_ctvrti.jpg", bounds).addTo(map);
+map.fitBounds(bounds); // Dopasowanie widoku mapy do obrazu
+
+// Funkcja przekształcająca piksele na współrzędne na mapie Leaflet
+function przeksztalcNaWspolrzedne(x, y) {
+  return [y, x]; // Leaflet używa współrzędnych w odwróconej kolejności
+}
+
+// function searchDistrict() {
+//   var searchTerm = document.getElementById("search").value.toLowerCase();
+
+//   // Usuwanie istniejących markerów
+//   map.eachLayer(function (layer) {
+//     if (layer instanceof L.Marker) {
+//       map.removeLayer(layer);
+//     }
+//   });
+
+//   // Jeśli pole wyszukiwania jest puste, zakończ funkcję
+//   if (!searchTerm) {
+//     return;
+//   }
+
+//   var found = false;
+
+//   // Przeszukiwanie bazy dzielnic
+//   for (var dzielnica in dzielnice) {
+//     if (dzielnica.toLowerCase().includes(searchTerm.toLowerCase())) {
+//       var coords = dzielnice[dzielnica].koordynaty;
+
+//       // Dodanie markera w znalezionej dzielnicy
+//       L.marker(coords).addTo(map).bindPopup(`<b>${dzielnica}</b>`).openPopup();
+//       map.setView(coords, 0); // Zoom na znalezioną dzielnicę
+//       found = true;
+//     }
+//   }
+// }
